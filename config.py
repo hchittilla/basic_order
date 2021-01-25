e68945ec09c33c85574f39fff53ff45a368abf77
hchittilla <hchittilla@gmail.com>
2021-01-25 12:27:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "88d2fb24-5854-4790-aea0-15a47ef6c30a")
+    APP_ID = os.environ.get("MicrosoftAppId", "bdb74de2-8f55-46dc-bbc0-61b5831d061f")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "AtLeastSixteenCharacters_0")
     LUIS_APP_ID = "25a9586a-daa4-437f-b3ab-b347d3f1f467"
     LUIS_API_KEY = "21b55404c0ea4531981fbd9865de699b"
